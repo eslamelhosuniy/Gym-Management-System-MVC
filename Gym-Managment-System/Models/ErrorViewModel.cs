e17c39b9f897458d569b61dafc85bb593b4c87f7
Eslam Elhosuniy <119428009+eslamelhosuniy@users.noreply.github.com>
2024-12-14 17:24:54 +0200

    Delete VS code Gym-Managment-System directory
    Delete VS code

--- Gym-Managment-System/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Gym_Managment_System.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
