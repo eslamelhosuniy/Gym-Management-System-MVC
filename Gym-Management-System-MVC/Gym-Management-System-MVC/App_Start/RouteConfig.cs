a6f01b0bbda5b757266950e5fecc34ddb7d21ea7
Hazem khaled <118244654+hazemkhaled551@users.noreply.github.com>
2024-12-18 19:50:24 +0200

    add views

--- Gym-Management-System-MVC/Gym-Management-System-MVC/App_Start/RouteConfig.cs
+++ Gym-Management-System-MVC/Gym-Management-System-MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Gym_Management_System_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "login", id = UrlParameter.Optional }
             );
         }
     }
